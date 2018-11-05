9d731dc4b3a321f42944905f041d522d86f3bc89
rim2011 <43763622+rim2011@users.noreply.github.com>
2018-11-05 11:25:50 +0100

    Add files via upload

---
+++ intro.php
@@ -0,0 +1,18 @@
+<!Doctype html>
+<html>
+<head>
+
+</head>
+<body>
+<?php
+echo 'Ceci est une ligne créée uniquement en PHP';?>
+<br>
+<?php
+echo  'Ceci est la 2ème phrase créée avec PHP';
+?>
+<br>
+<?php
+echo '<a href="https://gomycode.tn/" > gomycode </a>'; ?>
+</body>
+
+</html>
